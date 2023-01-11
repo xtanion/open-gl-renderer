3ed26a679dd4b8c011e1c2ffc0bc6fb148b252f6
xtanion <anandshivam54321@gmail.com>
2023-01-11 21:28:59 +0530

    some shit

--- headers/Mesh.h
+++ headers/Mesh.h
@@ -8,18 +8,28 @@
 #include "EBO.h"
 #include "Texture.h"
 #include "Camera.h"
+#include "../include/glm/glm.hpp"
+#include "../include/glm/gtc/matrix_transform.hpp"
 
-class Mesh{
+// struct Vertex {
+//     glm::vec3 Position;
+//     glm::vec3 Colors;
+//     glm::vec2 TexCoords;
+//     glm::vec3 Normal;
+// };
 
-    public:
-        std::vector <Vertex> vertices;
-        std::vector <GLuint> indices;
-        std::vector <Texture> textures;
+class Mesh
+{
 
-        VAO vao;
-        Mesh(std::vector<Vertex>& vertices, std::vector<GLuint>& indices, std::vector<Texture>& textures);
+public:
+    std::vector<Vertex> vertices;
+    std::vector<GLuint> indices;
+    std::vector<Texture> textures;
 
-        void Draw(Shader& shader, Camera& camera);
+    VAO vao;
+    Mesh(std::vector<Vertex> &vertices, std::vector<GLuint> &indices, std::vector<Texture> &textures);
+
+    void Draw(Shader &shader, Camera &camera);
 };
 
 #endif //MESH_H
